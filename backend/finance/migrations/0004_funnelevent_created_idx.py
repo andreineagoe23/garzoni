e3c6e009ad45b9e8acecb894845e46ff3d16e2db
Andrei Neagoe <neagoe.andrei23@yahoo.com>
2026-01-26 18:32:39 +0000

    fix migrations

--- backend/finance/migrations/0004_funnelevent_created_idx.py
+++ backend/finance/migrations/0004_funnelevent_created_idx.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("finance", "0003_subscriptionplan"),
+        ("finance", "0002_funnelevent_and_more"),
     ]
 
     operations = [
