ec3f57383d1e9d4b8d4a18c6a7c2a4eb7fd91660
Andrei Neagoe <neagoe.andrei23@yahoo.com>
2026-02-11 17:43:43 +0000

    fixes for dev

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -1,13 +1,5 @@
 from __future__ import absolute_import, unicode_literals
 
-# Use PyMySQL as MySQLdb so local dev works without mysqlclient C libs
-try:
-    import pymysql
-
-    pymysql.install_as_MySQLdb()
-except ImportError:
-    pass
-
 # This will make sure the app is always imported when
 # Django starts, so Celery tasks can be recognized.
 from .celery import app as celery_app
