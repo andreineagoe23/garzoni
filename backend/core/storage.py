84a8570ef95e83cab8560a6bed68c1e38b8f9674
Andrei Neagoe <neagoe.andrei23@yahoo.com>
2026-04-09 11:22:30 +0100

    admin fixes2

--- backend/core/storage.py
+++
@@ -1,6 +0,0 @@
-from whitenoise.storage import CompressedManifestStaticFilesStorage
-
-
-class NonStrictCompressedManifestStaticFilesStorage(CompressedManifestStaticFilesStorage):
-    """WhiteNoise storage that doesn't crash on missing CSS/JS references (e.g. Django 4.2 admin)."""
-    manifest_strict = False
