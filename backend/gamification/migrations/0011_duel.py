ca18ed7a67b21fdb546662266087a9fc3cd1d2ac
Andrei Neagoe <neagoe.andrei23@yahoo.com>
2026-05-26 10:37:50 +0300

    migration fixes

--- backend/gamification/migrations/0011_duel.py
+++ backend/gamification/migrations/0011_duel.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="Duel",
             fields=[
-                ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
                 (
                     "status",
                     models.CharField(
